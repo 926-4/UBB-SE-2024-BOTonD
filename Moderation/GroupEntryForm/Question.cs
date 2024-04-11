480fd5ed60a427bd1182765b6f012215b6feb73b
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-04-11 12:20:14 +0300

    Questions

---
+++ Moderation/GroupEntryForm/Question.cs
@@ -0,0 +1,40 @@
+﻿namespace Moderation.GroupEntryForm
+{
+    public interface IQuestion
+    {
+        string Text { set;  get; }
+    
+    }
+    public class TextQuestion : IQuestion
+    {
+        public string Text { get; set; }
+        public TextQuestion(String text)
+        {
+            Text = text;
+        }
+    }
+    public class SliderQuestion : IQuestion
+    {
+
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public string Text { get; set; }
+        public SliderQuestion(String text, int min, int max) 
+        {
+            Min = min;
+            Max = max;
+            Text = text;
+        }
+    }
+    public class RadioQuestion : IQuestion
+    {
+
+        public List<string> Options { get; set; }
+        public string Text { get; set; }
+        public RadioQuestion(String text, List<string> options)
+        {
+            Text = text;
+            Options = options;
+        }
+    }
+}
