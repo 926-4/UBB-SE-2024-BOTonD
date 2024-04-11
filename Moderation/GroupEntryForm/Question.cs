3db09d5f5017b3f941e04508f0d5e60e794e464b
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-04-11 16:39:36 +0300

    basic repository done

--- Moderation/GroupEntryForm/Question.cs
+++ Moderation/GroupEntryForm/Question.cs
@@ -1,4 +1,7 @@
 ﻿using CoreText;
+using System.Collections;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
 
 namespace Moderation.GroupEntryForm
 {
@@ -27,7 +30,67 @@ namespace Moderation.GroupEntryForm
     }
     public class QuestionRepository
     {
-        private List<Question> data;
+        private readonly Dictionary<Guid, Question> data = [];
+        public Question Get(Guid id)
+        {
+            return data[id];
+        }
+        public Collection<Question> GetAll()
+        {
+            return new Collection<Question>([.. data.Values]);
+        }
+        public bool Add(Question question)
+        {
+            return data.TryAdd(question.Id, question);
+        }
+        public bool Edit(Guid id, Question question)
+        {
+            if (!data.ContainsKey(id))
+                return false;
+            data[id] = question;
+            return true;
+        }
+        public bool Delete(Guid id)
+        {
+            return data.Remove(id);
+        }
+    }
+    public partial class QuestionFormPage : ContentPage
+    {
+        private QuestionRepository questionsRepo;
 
+        public QuestionFormPage(List<Question> questions)
+        {
+            InitializeComponent();
+            questionsRepo = new();
+            foreach(var question in questions)
+            {
+                questionsRepo.Add(question);
+            }
+            CreateForm();
+        }
+
+        private void CreateForm()
+        {
+            foreach (var question in questionsRepo.GetAll())
+            {
+                switch (question.GetType())
+                {
+                    case Type t when t == typeof(TextQuestion):
+                        var textEntry = new Entry();
+                        textEntry.Placeholder = question.Text;
+                        //stackLayout.Children.Add(textEntry);
+                        break;
+                    case Type t when t == typeof(Slider):
+                        var slider = new Slider();
+                        slider.Minimum = 0;
+                        slider.Maximum = 100;
+                        //stackLayout.Children.Add(slider);
+                        break;
+                    default:
+                        throw new Exception("Invalid question type");
+                }
+            }
+        }
     }
 }
