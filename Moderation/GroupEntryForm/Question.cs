2f8d375ff334cbf7f34286f4cc3f4e4f50c3e6f2
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-04-12 11:35:24 +0300

    Separated classes across multiple files Deleted the class creating the form in the gui because I probably did not create it right and will try ot do it again

--- Moderation/GroupEntryForm/Question.cs
+++ Moderation/GroupEntryForm/Question.cs
@@ -1,9 +1,4 @@
-﻿using CoreText;
-using System.Collections;
-using System.Collections.ObjectModel;
-using Microsoft.Maui.Controls;
-
-namespace Moderation.GroupEntryForm
+﻿namespace Moderation.GroupEntryForm
 {
     public abstract class Question
     {
@@ -13,84 +8,6 @@ namespace Moderation.GroupEntryForm
         public Question(string text)
         {
             Text = text;
-        }
-    }
-
-    public class TextQuestion(string text) : Question(text)
-    {
-    }
-    public class SliderQuestion(string text, int min, int max) : Question(text)
-    {
-        public int Min { get; } = min;
-        public int Max { get; } = max;
-    }
-    public class RadioQuestion(String text, List<string> options) : Question(text)
-    {
-        public List<string> Options { get; set; } = options;
-    }
-    public class QuestionRepository
-    {
-        private readonly Dictionary<Guid, Question> data = [];
-        public Question Get(Guid id)
-        {
-            return data[id];
-        }
-        public Collection<Question> GetAll()
-        {
-            return new Collection<Question>([.. data.Values]);
-        }
-        public bool Add(Question question)
-        {
-            return data.TryAdd(question.Id, question);
-        }
-        public bool Edit(Guid id, Question question)
-        {
-            if (!data.ContainsKey(id))
-                return false;
-            data[id] = question;
-            return true;
-        }
-        public bool Delete(Guid id)
-        {
-            return data.Remove(id);
-        }
-    }
-    public partial class QuestionFormPage : ContentPage
-    {
-        private QuestionRepository questionsRepo;
-
-        public QuestionFormPage(List<Question> questions)
-        {
-            InitializeComponent();
-            questionsRepo = new();
-            foreach(var question in questions)
-            {
-                questionsRepo.Add(question);
-            }
-            CreateForm();
-        }
-
-        private void CreateForm()
-        {
-            foreach (var question in questionsRepo.GetAll())
-            {
-                switch (question.GetType())
-                {
-                    case Type t when t == typeof(TextQuestion):
-                        var textEntry = new Entry();
-                        textEntry.Placeholder = question.Text;
-                        //stackLayout.Children.Add(textEntry);
-                        break;
-                    case Type t when t == typeof(Slider):
-                        var slider = new Slider();
-                        slider.Minimum = 0;
-                        slider.Maximum = 100;
-                        //stackLayout.Children.Add(slider);
-                        break;
-                    default:
-                        throw new Exception("Invalid question type");
-                }
-            }
         }
     }
 }
