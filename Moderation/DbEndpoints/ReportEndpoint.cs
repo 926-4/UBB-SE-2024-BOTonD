201b018f972f59e10178a9d80bbff64c67f22d94
SzilagyiBotond <ancelotti156@gmail.com>
2024-04-16 11:16:16 +0300

    finalendpoints

--- Moderation/DbEndpoints/ReportEndpoint.cs
+++ Moderation/DbEndpoints/ReportEndpoint.cs
@@ -5,7 +5,6 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Devices.Bluetooth.Advertisement;
 
 namespace Moderation.DbEndpoints
 {
