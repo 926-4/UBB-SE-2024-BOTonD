477b8b895df39e88a11987ee8af156e5fc78230b
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-04-16 12:03:33 +0300

    refactoring done I think compiler does not complain :D

--- Moderation/DbEndpoints/JoinRequestEndpoints.cs
+++ Moderation/DbEndpoints/JoinRequestEndpoints.cs
@@ -1,50 +1,41 @@
 ﻿using Moderation.Entities;
-using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Moderation.DbEndpoints
 {
     public class JoinRequestEndpoints
     {
-        private static string connectionString = "Server=tcp:iss.database.windows.net,1433;Initial Catalog=iss;Persist Security Info=False;User ID=iss;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private static readonly string connectionString = "Server=tcp:iss.database.windows.net,1433;Initial Catalog=iss;Persist Security Info=False;User ID=iss;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public static void CreateJoinRequest(JoinRequest joinRequest)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            // Insert JoinRequest entry
+            string insertJoinRequestSql = "INSERT INTO JoinRequest (Id, UserId) VALUES (@Id, @UserId)";
+            using (SqlCommand command = new(insertJoinRequestSql, connection))
             {
-                connection.Open();
+                command.Parameters.AddWithValue("@Id", joinRequest.Id);
+                command.Parameters.AddWithValue("@UserId", joinRequest.userId);
+                command.ExecuteNonQuery();
+            }
 
-                // Insert JoinRequest entry
-                string insertJoinRequestSql = "INSERT INTO JoinRequest (Id, UserId) VALUES (@Id, @UserId)";
-                using (SqlCommand command = new SqlCommand(insertJoinRequestSql, connection))
-                {
-                    command.Parameters.AddWithValue("@Id", joinRequest.Id);
-                    command.Parameters.AddWithValue("@UserId", joinRequest.userId);
-                    command.ExecuteNonQuery();
-                }
-
-                // Insert JoinRequestMessage entries
-                string insertJoinRequestMessageSql = "INSERT INTO JoinRequestMessage (JoinRequestId, [Key], [Value]) VALUES (@JoinRequestId, @Key, @Value)";
-                foreach (var kvp in joinRequest.messageResponse)
-                {
-                    using (SqlCommand command = new SqlCommand(insertJoinRequestMessageSql, connection))
-                    {
-                        command.Parameters.AddWithValue("@JoinRequestId", joinRequest.Id);
-                        command.Parameters.AddWithValue("@Key", kvp.Key);
-                        command.Parameters.AddWithValue("@Value", kvp.Value);
-                        command.ExecuteNonQuery();
-                    }
-                }
+            // Insert JoinRequestMessage entries
+            string insertJoinRequestMessageSql = "INSERT INTO JoinRequestMessage (JoinRequestId, [Key], [Value]) VALUES (@JoinRequestId, @Key, @Value)";
+            foreach (var kvp in joinRequest.messageResponse)
+            {
+                using SqlCommand command = new(insertJoinRequestMessageSql, connection);
+                command.Parameters.AddWithValue("@JoinRequestId", joinRequest.Id);
+                command.Parameters.AddWithValue("@Key", kvp.Key);
+                command.Parameters.AddWithValue("@Value", kvp.Value);
+                command.ExecuteNonQuery();
             }
         }
         public static List<JoinRequest> ReadAllJoinRequests()
         {
-            List<JoinRequest> joinRequests = new List<JoinRequest>();
+            List<JoinRequest> joinRequests = [];
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new(connectionString))
             {
                 connection.Open();
 
@@ -52,39 +43,35 @@ namespace Moderation.DbEndpoints
                              "FROM JoinRequest j " +
                              "JOIN JoinRequestMessage m ON j.Id = m.JoinRequestId";
 
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                using SqlCommand command = new(sql, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+                Guid currentJoinRequestId = Guid.Empty;
+                Dictionary<string, string> messageResponse = [];
+
+                while (reader.Read())
                 {
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    Guid joinRequestId = reader.GetGuid(0);
+
+                    if (joinRequestId != currentJoinRequestId)
                     {
-                        Guid currentJoinRequestId = Guid.Empty;
-                        Dictionary<string, string> messageResponse = new Dictionary<string, string>();
-
-                        while (reader.Read())
-                        {
-                            Guid joinRequestId = reader.GetGuid(0);
-
-                            if (joinRequestId != currentJoinRequestId)
-                            {
-                                if (currentJoinRequestId != Guid.Empty)
-                                {
-                                    JoinRequest joinRequest = new JoinRequest(currentJoinRequestId, messageResponse);
-                                    joinRequests.Add(joinRequest);
-                                    messageResponse = new Dictionary<string, string>();
-                                }
-
-                                currentJoinRequestId = joinRequestId;
-                            }
-
-                            messageResponse.Add(reader.GetString(2), reader.GetString(3));
-                        }
-
-                        // Add the last join request
                         if (currentJoinRequestId != Guid.Empty)
                         {
-                            JoinRequest joinRequest = new JoinRequest(currentJoinRequestId, messageResponse);
+                            JoinRequest joinRequest = new(currentJoinRequestId, messageResponse);
                             joinRequests.Add(joinRequest);
+                            messageResponse = [];
                         }
+
+                        currentJoinRequestId = joinRequestId;
                     }
+
+                    messageResponse.Add(reader.GetString(2), reader.GetString(3));
+                }
+
+                // Add the last join request
+                if (currentJoinRequestId != Guid.Empty)
+                {
+                    JoinRequest joinRequest = new(currentJoinRequestId, messageResponse);
+                    joinRequests.Add(joinRequest);
                 }
             }
 
@@ -93,23 +80,21 @@ namespace Moderation.DbEndpoints
 
         public static void DeleteJoinRequest(Guid joinRequestId)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            string deleteJoinRequestMessageSql = "DELETE FROM JoinRequestMessage WHERE JoinRequestId = @JoinRequestId";
+            using (SqlCommand command = new(deleteJoinRequestMessageSql, connection))
             {
-                connection.Open();
+                command.Parameters.AddWithValue("@JoinRequestId", joinRequestId);
+                command.ExecuteNonQuery();
+            }
 
-                string deleteJoinRequestMessageSql = "DELETE FROM JoinRequestMessage WHERE JoinRequestId = @JoinRequestId";
-                using (SqlCommand command = new SqlCommand(deleteJoinRequestMessageSql, connection))
-                {
-                    command.Parameters.AddWithValue("@JoinRequestId", joinRequestId);
-                    command.ExecuteNonQuery();
-                }
-
-                string deleteJoinRequestSql = "DELETE FROM JoinRequest WHERE Id = @Id";
-                using (SqlCommand command = new SqlCommand(deleteJoinRequestSql, connection))
-                {
-                    command.Parameters.AddWithValue("@Id", joinRequestId);
-                    command.ExecuteNonQuery();
-                }
+            string deleteJoinRequestSql = "DELETE FROM JoinRequest WHERE Id = @Id";
+            using (SqlCommand command = new(deleteJoinRequestSql, connection))
+            {
+                command.Parameters.AddWithValue("@Id", joinRequestId);
+                command.ExecuteNonQuery();
             }
         }
     }
--- Moderation/DbEndpoints/RoleEndpoints.cs
+++ Moderation/DbEndpoints/RoleEndpoints.cs
@@ -1,151 +1,123 @@
 ﻿using Moderation.Entities;
-using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Moderation.DbEndpoints
 {
-    
-    public  class RoleEndpoints
+
+    public class RoleEndpoints
     {
-        private static string connectionString = "Server=tcp:iss.database.windows.net,1433;Initial Catalog=iss;Persist Security Info=False;User ID=iss;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private static readonly string connectionString = "Server=tcp:iss.database.windows.net,1433;Initial Catalog=iss;Persist Security Info=False;User ID=iss;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public static void CreateRole(Role role)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+            string sql = "INSERT INTO UserRole VALUES (@Id,@N)";
+            using (SqlCommand command = new(sql, connection))
             {
-                connection.Open();
-                string sql = "INSERT INTO UserRole VALUES (@Id,@N)";
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@Id", role.Id);
-                    command.Parameters.AddWithValue("@N", role.Name);
-                    command.ExecuteNonQuery();
-                }
-                foreach (var permission in role.Permissions)
-                {
-                    sql = "Insert into RolePermission Values (@id,@p)";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        command.Parameters.AddWithValue("@Id", role.Id);
-                        command.Parameters.AddWithValue("@p", permission);
-                        command.ExecuteNonQuery();
-                    }
-                }
+                command.Parameters.AddWithValue("@Id", role.Id);
+                command.Parameters.AddWithValue("@N", role.Name);
+                command.ExecuteNonQuery();
+            }
+            foreach (var permission in role.Permissions)
+            {
+                sql = "Insert into RolePermission Values (@id,@p)";
+                using SqlCommand command = new(sql, connection);
+                command.Parameters.AddWithValue("@Id", role.Id);
+                command.Parameters.AddWithValue("@p", permission);
+                command.ExecuteNonQuery();
             }
         }
         public static List<Role> ReadRole()
         {
-            List<Role> roles = new List<Role>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            List<Role> roles = [];
+            using (SqlConnection connection = new(connectionString))
             {
                 connection.Open();
                 string sql = "SELECT * FROM UserRole r";
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                using SqlCommand command = new(sql, connection);
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    Role role = new                    (
+                        reader.GetGuid(0),
+                        reader.GetString(1)
+                    );
+
+                    // Fetch permissions for the current role from RolePermission table
+                    string rolePermissionSql = "SELECT Permission FROM RolePermission WHERE RoleId = @RoleId";
+                    using (SqlCommand rolePermissionCommand = new(rolePermissionSql, connection))
                     {
-                        while (reader.Read())
+                        rolePermissionCommand.Parameters.AddWithValue("@RoleId", role.Id);
+
+                        using SqlDataReader rolePermissionReader = rolePermissionCommand.ExecuteReader();
+                        while (rolePermissionReader.Read())
                         {
-                            Role role = new Role
-                            (
-                                reader.GetGuid(0),
-                                reader.GetString(1)
-                            );
-
-                            // Fetch permissions for the current role from RolePermission table
-                            string rolePermissionSql = "SELECT Permission FROM RolePermission WHERE RoleId = @RoleId";
-                            using (SqlCommand rolePermissionCommand = new SqlCommand(rolePermissionSql, connection))
-                            {
-                                rolePermissionCommand.Parameters.AddWithValue("@RoleId", role.Id);
-
-                                using (SqlDataReader rolePermissionReader = rolePermissionCommand.ExecuteReader())
-                                {
-                                    while (rolePermissionReader.Read())
-                                    {
-                                        // Map the permission string to the Permission enum
-                                        string permissionString = rolePermissionReader.GetString(0);
-                                        Permission permission = (Permission)Enum.Parse(typeof(Permission), permissionString);
-                                        role.Permissions.Add(permission);
-                                    }
-                                }
-                            }
-                            roles.Add(role);
+                            // Map the permission string to the Permission enum
+                            string permissionString = rolePermissionReader.GetString(0);
+                            Permission permission = (Permission)Enum.Parse(typeof(Permission), permissionString);
+                            role.Permissions.Add(permission);
                         }
                     }
+                    roles.Add(role);
                 }
             }
             return roles;
         }
         public static void UpdateRoleName(Guid roleId, string newName)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
 
-                string sql = "UPDATE UserRole SET Name = @NewName WHERE RoleId = @RoleId";
+            string sql = "UPDATE UserRole SET Name = @NewName WHERE RoleId = @RoleId";
 
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@NewName", newName);
-                    command.Parameters.AddWithValue("@RoleId", roleId);
+            using SqlCommand command = new(sql, connection);
+            command.Parameters.AddWithValue("@NewName", newName);
+            command.Parameters.AddWithValue("@RoleId", roleId);
 
-                    command.ExecuteNonQuery();
-                }
-            }
+            command.ExecuteNonQuery();
         }
         public static void UpdateRolePermissions(Guid roleId, List<Permission> newPermissions)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            // Delete existing permissions for the role
+            string deleteSql = "DELETE FROM RolePermission WHERE RoleId = @RoleId";
+            using (SqlCommand deleteCommand = new(deleteSql, connection))
             {
-                connection.Open();
+                deleteCommand.Parameters.AddWithValue("@RoleId", roleId);
+                deleteCommand.ExecuteNonQuery();
+            }
 
-                // Delete existing permissions for the role
-                string deleteSql = "DELETE FROM RolePermission WHERE RoleId = @RoleId";
-                using (SqlCommand deleteCommand = new SqlCommand(deleteSql, connection))
-                {
-                    deleteCommand.Parameters.AddWithValue("@RoleId", roleId);
-                    deleteCommand.ExecuteNonQuery();
-                }
-
-                // Insert new permissions for the role
-                string insertSql = "INSERT INTO RolePermission (RoleId, Permission) VALUES (@RoleId, @Permission)";
-                foreach (Permission permission in newPermissions)
-                {
-                    using (SqlCommand insertCommand = new SqlCommand(insertSql, connection))
-                    {
-                        insertCommand.Parameters.AddWithValue("@RoleId", roleId);
-                        insertCommand.Parameters.AddWithValue("@Permission", permission.ToString());
-                        insertCommand.ExecuteNonQuery();
-                    }
-                }
+            // Insert new permissions for the role
+            string insertSql = "INSERT INTO RolePermission (RoleId, Permission) VALUES (@RoleId, @Permission)";
+            foreach (Permission permission in newPermissions)
+            {
+                using SqlCommand insertCommand = new(insertSql, connection);
+                insertCommand.Parameters.AddWithValue("@RoleId", roleId);
+                insertCommand.Parameters.AddWithValue("@Permission", permission.ToString());
+                insertCommand.ExecuteNonQuery();
             }
         }
 
         public static void DeleteRole(Guid roleId)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            // Delete associated permissions from RolePermission table first
+            string deletePermissionSql = "DELETE FROM RolePermission WHERE RoleId = @RoleId";
+            using (SqlCommand deletePermissionCommand = new(deletePermissionSql, connection))
             {
-                connection.Open();
-
-                // Delete associated permissions from RolePermission table first
-                string deletePermissionSql = "DELETE FROM RolePermission WHERE RoleId = @RoleId";
-                using (SqlCommand deletePermissionCommand = new SqlCommand(deletePermissionSql, connection))
-                {
-                    deletePermissionCommand.Parameters.AddWithValue("@RoleId", roleId);
-                    deletePermissionCommand.ExecuteNonQuery();
-                }
-
-                // Delete role from UserRole table after deleting associated permissions
-                string deleteRoleSql = "DELETE FROM UserRole WHERE RoleId = @RoleId";
-                using (SqlCommand deleteRoleCommand = new SqlCommand(deleteRoleSql, connection))
-                {
-                    deleteRoleCommand.Parameters.AddWithValue("@RoleId", roleId);
-                    deleteRoleCommand.ExecuteNonQuery();
-                }
+                deletePermissionCommand.Parameters.AddWithValue("@RoleId", roleId);
+                deletePermissionCommand.ExecuteNonQuery();
             }
+
+            // Delete role from UserRole table after deleting associated permissions
+            string deleteRoleSql = "DELETE FROM UserRole WHERE RoleId = @RoleId";
+            using SqlCommand deleteRoleCommand = new(deleteRoleSql, connection);
+            deleteRoleCommand.Parameters.AddWithValue("@RoleId", roleId);
+            deleteRoleCommand.ExecuteNonQuery();
         }
     }
 }
