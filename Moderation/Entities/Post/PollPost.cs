8460e2043372b9ea55b0030d2e0009a784c91761
SzilagyiBotond <ancelotti156@gmail.com>
2024-04-18 02:59:43 +0300

    Poll endpoints

--- Moderation/Entities/Post/PollPost.cs
+++ Moderation/Entities/Post/PollPost.cs
@@ -1,16 +1,19 @@
 ﻿namespace Moderation.Entities.Post
 {
-    public class PollPost(Guid postId, string content, User author, int score, string status, List<string> options, List<Award> awards, bool isDeleted = false) : IPost
+    public class PollPost(Guid postId, string content, GroupUser author, int score, string status, List<string> options, List<Award> awards, Guid groupId, bool isDeleted = false) : IPost
     { 
         public Guid Id { get; set; } = postId;
         public string Content { get; set; } = content;
-        public User Author { get; set; } = author;
+        public GroupUser Author { get; set; } = author;
         public int Score { get; set; } = score;
         public string Status { get; set; } = status;
         public bool IsDeleted { get; set; } = isDeleted;
         public List<Award> Awards { get; set; } = awards;
         public List<String> Options { get; set; } = options;
         public Dictionary<Guid, Guid> Votes { get; set; } = [];
+
+        public Guid GroupId { get; set; } = groupId;
+
 
         public void AddOption(string option)
         {
