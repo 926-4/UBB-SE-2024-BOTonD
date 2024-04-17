3fa0b1b68d5c884b950758790714bbae51ba3f47
Sirbu Ciprian <ciprisbu21@gmail.com>
2024-04-18 00:27:05 +0300

    Fix

---
+++ Moderation/Entities/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moderation.Entities
+{
+    public class Question
+    {
+        public Guid QuestionId { get; set; }
+        public Guid RequestId { get; set; }
+        public string Type { get; set; }
+        public string Text { get; set; }
+    }
+}
