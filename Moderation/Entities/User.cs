1fb59be11bde46d4aef2cc809b55abb0f714c45c
Ioan Robert Scumpu <robertscumnpu@gmail.com>
2024-04-18 03:04:34 +0300

    Update User.cs

--- Moderation/Entities/User.cs
+++ Moderation/Entities/User.cs
@@ -10,11 +10,22 @@ namespace Moderation.Entities
     public class User : IHasID
     {
         public Guid Id { get; set; }
-        
         public string Username { get; set; }
         public string Password { get; set; }
 
-        public User(string username,string password)
+        public User(string username)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Password = GetRandomlyGeneratedPassword();
+        }
+        public User(Guid id, string username)
+        {
+            Id = id;
+            Username = username;
+            Password = GetRandomlyGeneratedPassword();
+        }
+        public User(string username, string password)
         {
             Id = Guid.NewGuid();
             Username = username;
@@ -25,6 +36,23 @@ namespace Moderation.Entities
             Id = id;
             Username = username;
             Password = password;
+        }
+        private string GetRandomlyGeneratedPassword()
+        {
+            Random random = new Random();
+
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+
+            int length = random.Next(8, 24);
+
+            char[] chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+
+            return new string(chars);
         }
     }
 }
