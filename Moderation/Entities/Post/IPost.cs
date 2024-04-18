ca7474bf546ba5ce4355f9773308f11c16368fff
Ioan Robert Scumpu <robertscumnpu@gmail.com>
2024-04-18 04:23:06 +0300

    Revert "Update IPost.cs"
    This reverts commit a1e7d3bbbbe45d3fbd55dab44a63a46ea793632a.

--- Moderation/Entities/Post/IPost.cs
+++ Moderation/Entities/Post/IPost.cs
@@ -5,7 +5,7 @@ namespace Moderation.Entities.Post
     public interface IPost : IHasID
     {
         string Content { get; set; }
-        GroupUser Author { get; set; }
+        User Author { get; set; }
         int Score { get; set; }
         string Status { get; set; }
         List<Award> Awards { get; set; }
