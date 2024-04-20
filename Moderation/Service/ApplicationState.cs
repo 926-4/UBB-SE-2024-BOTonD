607eef25b37a6358b3e9651aad8621e933d1de1b
Sirbu Ciprian <ciprisbu21@gmail.com>
2024-04-20 23:22:12 +0300

    IncludedReposInAppState

--- Moderation/Service/ApplicationState.cs
+++ Moderation/Service/ApplicationState.cs
@@ -1,5 +1,6 @@
 ﻿using Moderation.Authentication;
 using Moderation.CurrentSessionNamespace;
+using Moderation.Entities;
 using Moderation.Entities.Post;
 using Moderation.GroupEntryForm;
 using Moderation.GroupRulesView;
@@ -17,6 +18,16 @@ namespace Moderation.Serivce
         public GroupRepository Groups { get; } = new();
         public UserRepository UserRepository { get; } = new();
         public Repository<IPost> Posts { get; } = new();
+        public AwardRepository Awards { get; } = new();
+        public GroupRules Rules { get; } = new();
+        public GroupUserRepository GroupUsers { get; } = new();
+        public JoinRequestAnswerForOneQuestionRepository JoinRequestForOneQuestionAnswers { get; } = new();
+        public JoinRequestRepository JoinRequests { get; } = new();
+        public QuestionRepository Questions { get; } = new();
+        public ReportRepository Reports { get; } = new();
+        public RoleRepository Roles { get; } = new();
+        public TextPostRepository TextPosts { get; } = new();
+        public VoteRepository Votes { get; } = new();
         private ApplicationState InitialiseUsers()
         {
             /// Read data from DB
