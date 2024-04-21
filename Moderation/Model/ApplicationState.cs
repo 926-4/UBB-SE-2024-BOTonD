fabc3de27adf4a58c40e3304e9410d2a1c2c4c3e
Ioan Robert Scumpu <robertscumnpu@gmail.com>
2024-04-21 12:22:19 +0300

    Update ApplicationState.cs

--- Moderation/Model/ApplicationState.cs
+++ Moderation/Model/ApplicationState.cs
@@ -20,61 +20,10 @@ namespace Moderation.Model
         public Repository<IPost> Posts { get; } = new();
         private ApplicationState InitialiseUsers()
         {
-            /// TODO Move to repository/db
-            User victor = new("Victor");
-            string victorsPassword = "a";
-            User boti = new("Boti");
-            string botisPassword = "a";
-            User norby = new("Norby");
-            string norbysPassword = "a";
-            User cipri = new("Cipri");
-            string ciprisPassword = "a"; 
-            User ioan = new("Ioan");
-            string ioansPassword = "a";
-            Authenticator.AddUser(victor, victorsPassword);
-            Authenticator.AddUser(boti, botisPassword);
-            Authenticator.AddUser(norby, norbysPassword);
-            Authenticator.AddUser(cipri, ciprisPassword);
-            Authenticator.AddUser(ioan, ioansPassword);
-            return InitialiseGroups([victor,boti]);
+            return this;
         }
         private ApplicationState InitialiseGroups(List<User> users)
         {
-            Random rand = new();
-            var randomUserIndex = rand.Next(users.Count);
-            
-            Group group = new("Jimmy's borthday party", "None", users[randomUserIndex]);
-            Question q1 = new TextQuestion("What presend do you want to give me?");
-            Question q2 = new SliderQuestion("How much do you like me?:->", 0, 10);
-            Question q3 = new RadioQuestion("What do you want to eat?", ["grill", "pizza", "nothing", "vegetarian"]);
-            
-            Rule r1 = new(Guid.NewGuid(), "Vibe Check", "I do solemnly swear to be cool and chill");
-            Rule r2 = new(Guid.NewGuid(), "Vibe Check", "I do solemnly swear to be cool and chill");
-            Rule r3 = new(Guid.NewGuid(), "Vibe Check Check", "I do solemnly swear I won't ask why the first rule is written twice");
-            
-            group.GroupEntryQuestions.Add(q1.Id, q1);
-            group.GroupEntryQuestions.Add(q2.Id, q2); 
-            group.GroupEntryQuestions.Add(q3.Id, q3);
-            group.GroupRules.Add(r1.Id, r1);
-            group.GroupRules.Add(r2.Id, r2);
-            group.GroupRules.Add(r3.Id, r3);
-            Groups.Add(group.Id, group);
-            
-            randomUserIndex = rand.Next(users.Count);
-            group = new("Study session @ Jacks", "None", users[randomUserIndex]);
-            q1 = new TextQuestion("What time are you free at?");
-            q2 = new RadioQuestion("What are you studying?", ["biology", "maths", "physics", "literature"]);
-            q3 = new TextQuestion("Any other mentions? (leave free if all's good)");
-            r1 = new(Guid.NewGuid(), "Kindness Check", "I will do my best to help others during my stay here");
-            r2 = new(Guid.NewGuid(), "Manners Check", "I will not throw a spontaneous party");
-
-            group.GroupEntryQuestions.Add(q1.Id, q1);
-            group.GroupEntryQuestions.Add(q2.Id, q2);
-            group.GroupEntryQuestions.Add(q3.Id, q3);
-            group.GroupRules.Add(r1.Id, r1);
-            group.GroupRules.Add(r2.Id, r2);
-            Groups.Add(group.Id, group);
-            
             return this;
         }
         static public ApplicationState Get()
